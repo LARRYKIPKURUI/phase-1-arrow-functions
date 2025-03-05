d3d8b75711f8b471e7902cb1320cdcc06f596f07
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-05 21:37:53 +0300

    Arroe functions

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide = () => {
+    let a = 2000
+    let b = 100
+    return a/b
+}
+
+const square = ()=> {
+    let x = 2
+
+    return x ** 2
+}
+
+const add = () => {
+    let a = 3
+    let b = 4
+
+    return a + b
+
+}
